e233de314e84f166ac7b0124b09cdf66fbe05090
NeacsuArmand <lord.evolution@yahoo.com>
2017-05-08 13:21:21 +0300

    V1 (visual studio)

---
+++ atm system C# with GUI/Deposit.cs
@@ -0,0 +1,165 @@
+
+using System.Diagnostics;
+using System;
+using System.Xml.Linq;
+using System.Windows.Forms;
+using System.Collections;
+using System.Drawing;
+using Microsoft.VisualBasic;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace atmsystem
+{
+	public partial class Deposit
+	{
+		public Deposit()
+		{
+			InitializeComponent();
+			
+		}
+		
+#region Default Instance
+		
+		private static Deposit defaultInstance;
+		
+		
+		public static Deposit Default
+		{
+			get
+			{
+				if (defaultInstance == null)
+				{
+					defaultInstance = new Deposit();
+					defaultInstance.FormClosed += new FormClosedEventHandler(defaultInstance_FormClosed);
+				}
+				
+				return defaultInstance;
+			}
+		}
+		
+		static void defaultInstance_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			defaultInstance = null;
+		}
+		
+#endregion
+		System.Data.OleDb.OleDbDataAdapter da = new System.Data.OleDb.OleDbDataAdapter();
+		System.Data.OleDb.OleDbConnection con = new System.Data.OleDb.OleDbConnection();
+		DataSet dset = new DataSet();
+		System.Data.OleDb.OleDbCommand cmd = new System.Data.OleDb.OleDbCommand();
+		string balance;
+		int num1;
+		int num2;
+		int total;
+		
+		public void Deposit_Load(System.Object sender, System.EventArgs e)
+		{
+			lbldate.Text = DateTime.Now.ToString();
+			lblaccno.Text = Mainmenu.Default.lblaccno.Text;
+		}
+		
+		public void btnok_Click(System.Object sender, System.EventArgs e)
+		{
+			string sql = default(string);
+			DataTable Log_in = new DataTable();
+			
+			try
+			{
+				
+				
+				
+				con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + "\\ATMsystem.accdb";
+				sql = "SELECT * FROM tblinfo where  account_no = " + lblaccno.Text + "";
+				
+				cmd.Connection = con;
+				cmd.CommandText = sql;
+				da.SelectCommand = cmd;
+				da.Fill(Log_in);
+				if (Log_in.Rows.Count > 0)
+				{
+					balance = (string) (Log_in.Rows[0]["balance"]);
+					num1 = int.Parse(balance);
+					num2 = int.Parse(txtamount.Text);
+					
+					if (num2 > 25000)
+					{
+						MessageBox.Show("Poti depozita maxim 25.000 lei!");
+					}
+					else if (num2 < 200)
+					{
+						MessageBox.Show("Depozitul minim este de 200 lei.");
+						
+						
+					}
+					else
+					{
+						total = num1 + num2;
+						
+						Receipt.Default.Show();
+						
+						Receipt.Default.lblbal.Text = balance;
+						Receipt.Default.Label3.Hide();
+						Receipt.Default.lblwith.Hide();
+						Receipt.Default.lbldep.Text = num2.ToString();
+						Receipt.Default.lblnewbal.Text = total.ToString();
+						
+						Receipt.Default.Label5.Show();
+						Receipt.Default.Label6.Show();
+						
+						
+						Receipt.Default.lblbal.Show();
+						Receipt.Default.Label3.Hide();
+						Receipt.Default.lblwith.Hide();
+						Receipt.Default.lbldep.Show();
+						Receipt.Default.lblnewbal.Show();
+						Receipt.Default.lblname.Text = Mainmenu.Default.lblname.Text;
+						this.Hide();
+						
+						
+					}
+				}
+				else
+				{
+					
+					
+				}
+				
+				
+				
+				
+				
+				
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Te rugam sa introduci cantitatea!");
+			}
+			txtamount.Text = "";
+			
+			
+			
+		}
+		
+		public void LinkLabel1_LinkClicked(System.Object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+		{
+			Mainmenu.Default.Show();
+			this.Hide();
+			
+		}
+		
+		public void txtamount_TextChanged(System.Object sender, System.EventArgs e)
+		{
+			
+		}
+		
+		public void lbldate_Click(System.Object sender, System.EventArgs e)
+		{
+			
+		}
+	}
+	
+}
---
+++ atm system C# with GUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,49 @@
+// VBConversions Note: VB project level imports
+using System.Diagnostics;
+using System;
+using System.Xml.Linq;
+using System.Windows.Forms;
+using System.Collections;
+using System.Drawing;
+using Microsoft.VisualBasic;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+// End of VB project level imports
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly:AssemblyTitle("atmsystem")]
+[assembly:AssemblyDescription("")]
+[assembly:AssemblyCompany("")]
+[assembly:AssemblyProduct("atmsystem")]
+[assembly:AssemblyCopyright("Copyright ©  2014")]
+[assembly:AssemblyTrademark("")]
+
+[assembly:ComVisible(false)]
+
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly:Guid("8f2fe687-19c9-4c8e-bcb1-557e95498980")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// <Assembly: AssemblyVersion("1.0.*")>
+
+[assembly:AssemblyVersion("1.0.0.0")]
+[assembly:AssemblyFileVersion("1.0.0.0")]
+
